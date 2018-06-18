b15162f8297d32ff04ec99fa64ba89a640da5123
Bentley Davis <bentley.davis@invitae.com>
2018-06-17 19:19:29 -0500

    example of child contexts

--- src/App.js
+++ src/App.js
@@ -17,44 +17,46 @@ class App extends Component {
     this.state = {
       rootClaimId: "1",
       claims: [
-        { id: "1", content: "Claim 1" },
-        { id: "1.1", content: "Claim 1.1" },
-        { id: "1.2", content: "Claim 1.2" },
-        { id: "1.1.1", content: "Claim 1.1.1" },
-        { id: "1.1.2", content: "Claim 1.1.2" },
-        { id: "2", content: "Claim 2" }
+        { id: "1", content: "Tabs are better than spaces" },
+        { id: "1.1", content: "Tabs require fewer charachters" },
+        { id: "1.2", content: "Developers that use spaces make more money" },
+        { id: "1.1.1", content: "Acme is building small IOT devices so every character counts" },
+        { id: "1.2.1", content: "John is looking for a high paying job" },
+        { id: "acme", content: "Should Acme standardize on tabs" },
+        { id: "john", content: "Should John learn to code with tabs" }
       ],
       edges: [
-        { parentId: "1", childId: "1.1", align: "pro", contextId: "1.1"}
-        , { parentId: "1", childId: "1.2", align: "con", contextId: "1.2"}
-        , { parentId: "2", childId: "1.1", align: "con", contextId: "1.1"}
-        , { parentId: "1.1", childId: "1.1.1", align: "pro", contextId: "1"}
-        , { parentId: "1.1", childId: "1.1.2", align: "pro", contextId: "2"}
+        { parentId: "1", childId: "1.1", align: "pro", contextId: "1" },
+        { parentId: "1", childId: "1.2", align: "con", contextId: "1" },
+        { parentId: "1.1", childId: "1.1.1", align: "pro", contextId: "acme" },
+        { parentId: "1.2", childId: "1.2.1", align: "pro", contextId: "john" },
+        { parentId: "acme", childId: "1", align: "pro", contextId: "acme" },
+        { parentId: "john", childId: "1", align: "pro", contextId: "john" }
       ],
       viewState: {
         claims: [
           { id: "1", open: true },
-          { id: "2", open: true },
+          { id: "1.2", open: true },
           { id: "1.1", open: true }
         ]
       }
     };
   }
 
-  getClaimView(claim) {
-    let claimView = this.state.viewState.claims.filter(c => c.id === claim.id)[0];
-    if (claimView === undefined) {
-      claimView = {}
-    }
-    return claimView;
-  }
+  // getClaimView(claim) {
+  //   let claimView = this.state.viewState.claims.filter(c => c.id === claim.id)[0];
+  //   if (claimView === undefined) {
+  //     claimView = {}
+  //   }
+  //   return claimView;
+  // }
 
   getClaimData(id) {
     const claim = this.state.claims.filter(claim => claim.id === id)[0]
 
     const claimData = {
       claim: claim,
-      claimView: this.getClaimView(claim),
+      //claimView: this.getClaimView(claim),
       ancestors: []
     }
 
@@ -67,11 +69,11 @@ class App extends Component {
     const parentClaim = parentClaimData.claim;
     const childEdges = this.state.edges.filter(edge => edge.parentId === parentClaim.id
       && (parentClaimData.ancestors.includes(edge.contextId)
-        || edge.contextId == edge.childId)
+        || edge.contextId == parentClaim.id)
     );
-    const parentClaimView = this.getClaimView(parentClaim);
+///    const parentClaimView = this.getClaimView(parentClaim);
     if (childEdges.length > 0
-      && parentClaimView.open
+//      && parentClaimView.open
     ) {
       const renderedChildren = childEdges.map((edge, step) => {
         const childData = this.getClaimData(edge.childId);
