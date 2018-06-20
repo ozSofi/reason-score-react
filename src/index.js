38915a46ba0006b47f361d2081f235ff94b1ce62
Bentley Davis <Bentley@BentleyDavis.com>
2018-06-20 08:47:42 -0500

    add build folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-//debugger;
-const claims = document.getElementsByTagName("claim");
 
-for (const claim of claims){
-    ReactDOM.render(<App
-        claimId={claim.getAttribute("claimId")}
-        claimData={claim.getAttribute("claimData")}
+const claims = document.getElementsByTagName('claim');
+
+for (const claim of claims) {
+  ReactDOM.render(<App
+    claimId={claim.getAttribute('claimId')}
+    claimData={claim.getAttribute('claimData')}
         />, claim);
 }
 
-//ReactDOM.render(<App />, Element.getElementsByTagName("claim"));
+// ReactDOM.render(<App />, Element.getElementsByTagName("claim"));
 registerServiceWorker();
