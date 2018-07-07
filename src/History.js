f00a57a3ec64b86eb1793e232d6cc7b0fde9d0cb
Bentley Davis <Bentley@BentleyDavis.com>
2018-07-07 18:34:04 -0500

    displays history dates

--- src/History.js
+++ src/History.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 function History(props) {
-    const items = Object.values(props.data.items).filter(item =>
-        item.type === 'act').reverse();
+    // const items = Object.values(props.data.items).filter(item =>
+    //     item.type === 'act').reverse();
+    var whens = Object.values(props.data.items).reduce(function (uniqueWhens, node) {
+        if (!uniqueWhens.includes(node.start)) {
+            uniqueWhens.push(node.start);
+        }
+        return uniqueWhens;
+    }, []);
+
+
     return (
         <div className='history'>
-            {items.map((history) => (
-                <div key={history.ver}>{history.ver}</div>
+            {whens.map((history) => (
+                <div key={history}>{history}</div>
             ))}
         </div>
     );
